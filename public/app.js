aefc2ada9770194950e1f5b7fcd53f3107a64631
Hao Chen <mmkrty@gmail.com>
2023-01-26 14:55:56 +0100

    add list template for rendering html template

--- public/app.js
+++ public/app.js
@@ -1,17 +1,6 @@
 import { Invoice } from "./classes/Invoice.js";
+import { ListTemplate } from "./classes/ListTemplate.js";
 import { Payment } from "./classes/Payment.js";
-let docOne;
-let docTwo;
-docOne = new Invoice("yoshi", "webwork", 250);
-docTwo = new Payment("mario", "plumbing work", 200);
-let docs = [];
-docs.push(docOne);
-docs.push(docTwo);
-const invOne = new Invoice("mario", "work on mario web", 250);
-const invTwo = new Invoice("luigi", "work on luigi web", 250);
-const invoices = [];
-invoices.push(invOne);
-invoices.push(invTwo);
 const anchor = document.querySelector("a");
 //form
 const form = document.querySelector(".new-item-form");
@@ -20,6 +9,9 @@ const type = document.querySelector("#type");
 const tofrom = document.querySelector("#tofrom");
 const details = document.querySelector("#details");
 const amount = document.querySelector("#amount");
+//list template instance
+const ul = document.querySelector("ul");
+const list = new ListTemplate(ul);
 form.addEventListener("submit", (e) => {
     e.preventDefault();
     let doc;
@@ -29,5 +21,5 @@ form.addEventListener("submit", (e) => {
     else {
         doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
     }
-    console.log(doc);
+    list.render(doc, type.value, "end");
 });
--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,7 @@
 import { Invoice } from "./classes/Invoice.js";
+import { ListTemplate } from "./classes/ListTemplate.js";
 import { Payment } from "./classes/Payment.js";
 import { HasFormatter } from "./interfaces/HasFormatter.js";
-
-let docOne: HasFormatter;
-let docTwo: HasFormatter;
-
-docOne = new Invoice("yoshi", "webwork", 250);
-docTwo = new Payment("mario", "plumbing work", 200);
-
-let docs: HasFormatter[] = [];
-docs.push(docOne);
-docs.push(docTwo);
-
-const invOne = new Invoice("mario", "work on mario web", 250);
-const invTwo = new Invoice("luigi", "work on luigi web", 250);
-
-const invoices: Invoice[] = [];
-invoices.push(invOne);
-invoices.push(invTwo);
 
 const anchor = document.querySelector("a")!;
 
@@ -30,6 +14,10 @@ const tofrom = document.querySelector("#tofrom") as HTMLInputElement;
 const details = document.querySelector("#details") as HTMLInputElement;
 const amount = document.querySelector("#amount") as HTMLInputElement;
 
+//list template instance
+const ul = document.querySelector("ul")!;
+const list = new ListTemplate(ul);
+
 form.addEventListener("submit", (e: Event) => {
   e.preventDefault();
 
@@ -40,5 +28,6 @@ form.addEventListener("submit", (e: Event) => {
   } else {
     doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
   }
-  console.log(doc);
+
+  list.render(doc, type.value, "end");
 });
